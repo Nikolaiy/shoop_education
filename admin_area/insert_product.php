0699d2730890a643e83b0cc5d828406a58f2b3aa
Nikolaiy <30789678+Nikolaiy@users.noreply.github.com>
2019-04-20 23:43:29 +0500

    Added to the file .. code to add the product to the database

--- admin_area/insert_product.php
+++ admin_area/insert_product.php
@@ -73,7 +73,7 @@
                         <div class="form-group">
                             <label for="" class="col-md-3 control-label"> Category </label>
                             <div class="col-md-6">
-                                <select name="product_category" class="form-control">
+                                <select name="category" class="form-control">
                                     <option> Select a Category  </option>
                                     <?php
                                         $get_cat = "select * from categories";
@@ -109,7 +109,7 @@
                         <div class="form-group">
                             <label for="" class="col-md-3 control-label"> Product Image 3 </label>
                             <div class="col-md-6">
-                                <input name="product_img3" type="file" class="form-control" required>
+                                <input name="product_img3" type="file" class="form-control form-height-custom"  required>
                             </div>
                         </div>
 
@@ -117,6 +117,13 @@
                             <label for="" class="col-md-3 control-label"> Product Price </label>
                             <div class="col-md-6">
                                 <input name="product_price" type="text" class="form-control" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="col-md-3 control-label"> Product Keywords </label>
+                            <div class="col-md-6">
+                                <input name="product_keywords" type="text" class="form-control" required>
                             </div>
                         </div>
 
@@ -145,8 +152,63 @@
 
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script src="https://cloud.tinymce.com/5/tinymce.min.js"></script>
+    <script>console.log('Привет')</script>
+    <!-- <script src="https://cloud.tinymce.com/5/tinymce.min.js"></script>
     <script>tinymce.init({ selector:'textarea' });</script>
 
 </body>
 </html>
+
+
+<?php
+
+    if(isset($_POST['submit'])){
+
+        $product_title = $_POST['product_title'];
+        $product_category = $_POST['product_category'];
+        $category = $_POST['category'];
+        $product_price = $_POST['product_price'];
+        $product_keywords = $_POST['product_keywords'];
+        $product_desc = $_POST['product_desc'];
+    
+        $product_img1 = $_FILES['product_img1']['name'];
+        $product_img2 = $_FILES['product_img2']['name'];
+        $product_img3 = $_FILES['product_img3']['name'];
+
+        $temp_name1 = $_FILES['product_img1']['tmp_name'];
+        $temp_name2 = $_FILES['product_img2']['tmp_name'];
+        $temp_name3 = $_FILES['product_img3']['tmp_name'];
+
+        move_uploaded_file($temp_name1, "product_images/$product_img1");
+        move_uploaded_file($temp_name2, "product_images/$product_img2");
+        move_uploaded_file($temp_name3, "product_images/$product_img3");
+
+        $insert_product = "insert into products (p_car_id, cat_id, date, product_title, product_img1, product_img2, product_img3, product_price, product_keywords, product_desc) values ('$product_category', '$category', NOW(), '$product_title', '$product_img1', '$product_img2', '$product_img3', '$product_price', '$product_keywords', '$product_desc')";
+
+        $run_product = mysqli_query($con, $insert_product);
+
+        if($run_product){
+
+            echo "<script>alert('Product has been inserted sucessfully')</script>";
+            echo "<script> window.open('insert_product.php', '_self') </script>";
+
+        }
+    
+    }
+
+?>
+
+<!-- // # sourceMappingURL=http://example.com/path/to/your/sourcemap.map -->
+
+
+
+
+
+
+
+
+
+
+
+
+
